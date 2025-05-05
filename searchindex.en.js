532ff97d7545f718f8be32999f26a94c6541ac0f
Cormac Lennon <cmpclenn@gmail.com>
2025-05-05 19:57:46 +0100

    Test

---
+++ searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Beatriz Severes.io \u003e \rPosts",
+    "content": "Introduction This is a post on Bea’s very cool Website.\nVisit the maker Cormac!",
+    "description": "Introduction This is a post on Bea’s very cool Website.\nVisit the maker Cormac!",
+    "tags": [],
+    "title": "Beas first post",
+    "uri": "/posts/my-first-post/index.html"
+  },
+  {
+    "breadcrumb": "Beatriz Severes.io",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Beatriz Severes.io",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Beatriz Severes.io",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
